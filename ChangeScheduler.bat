508752b64ae74dfef5e212ab2c75d0063e9f9734
iMajesticButter <bl.wolenetz@gmail.com>
2017-09-20 15:19:26 -0700

    scheduled commit

---
+++ ChangeScheduler.bat
@@ -0,0 +1,5 @@
+cd "D:\User\Documents\GitHub\iMajesticButter.github.io"
+"D:\Programs\Git\bin\git.exe" add .
+"D:\Programs\Git\bin\git.exe" commit -am "scheduled commit"
+"D:\Programs\Git\bin\git.exe" push
+pause
