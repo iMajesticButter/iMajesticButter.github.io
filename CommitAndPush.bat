2da9d07645f60e1c37bc742781ac2e73455b8a7d
iMajesticButter <bl.wolenetz@gmail.com>
2017-09-21 16:10:09 -0700

    scheduled commit

--- CommitAndPush.bat
+++ CommitAndPush.bat
@@ -1,6 +1,4 @@
-at 15:28
 cd "D:\User\Documents\GitHub\iMajesticButter.github.io"
 "D:\Programs\Git\bin\git.exe" add .
 "D:\Programs\Git\bin\git.exe" commit -am "scheduled commit"
 "D:\Programs\Git\bin\git.exe" push
-pause
